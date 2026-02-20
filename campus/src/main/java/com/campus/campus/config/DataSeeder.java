3f323431ae9fb8a3f570c0e6a1343c7e48d7e168
SudharsanVelmuruganM <sudharsanvelmurugan004@gmail.com>
2026-02-20 13:14:36 +0530

    Added postgres driver and fixed DB URL

--- campus/src/main/java/com/campus/campus/config/DataSeeder.java
+++ campus/src/main/java/com/campus/campus/config/DataSeeder.java
@@ -14,7 +14,7 @@ import com.campus.campus.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
